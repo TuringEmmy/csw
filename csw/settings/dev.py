c41db1c48cf859602fd2c2ee8e0e0ea5d2a5ff1c
TuringEmmy <yonglonggeng@163.com>
2019-02-20 22:06:08 -0500

    add logs folder

--- csw/settings/dev.py
+++ csw/settings/dev.py
@@ -106,11 +106,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-# LANGUAGE_CODE = 'en-us'
-# 修改成国内的时区和语言
-LANGUAGE_CODE = 'zh-hans'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Shanghai'
+TIME_ZONE = 'UTC'
+# 修改成国内的时区和语言
+# LANGUAGE_CODE = 'zh-hans'
+# TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
